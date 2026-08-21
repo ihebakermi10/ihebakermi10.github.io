0507f88cb4f4154d052265253da06f54372ad139
Iheb Akermi <iheb.akermi@elyosdigital.com>
2026-08-21 15:29:41 +0100

    Redesign portfolio: dark purple theme, code refactor, cat mascot
    Rebuilt static export with the dark charcoal-purple theme, new type
    system, particle background, oneko cat mascot, CircularText contrast
    fix, and Eggmed moved to the first project slot.

---
+++ _next/static/chunks/pages/components/OnekoCat-6719f55f60f0467b.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[581],{2206:function(n,_,u){(window.__NEXT_P=window.__NEXT_P||[]).push(["/components/OnekoCat",function(){return u(4981)}])}},function(n){n.O(0,[888,774,179],function(){return n(n.s=2206)}),_N_E=n.O()}]);
---
+++ _next/static/chunks/pages/components/ParticleField-480fa9cebc37d767.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[656],{5301:function(n,_,u){(window.__NEXT_P=window.__NEXT_P||[]).push(["/components/ParticleField",function(){return u(2269)}])}},function(n){n.O(0,[888,774,179],function(){return n(n.s=5301)}),_N_E=n.O()}]);
